4332dfa44593c4121aa8c9dd344c676619391519
caomanhlinh1997-svg <caomanhlinh1997@gmail.com>
2025-12-01 00:25:51 +0700

    12:30 AM - 01.12.2025
    Bản có menu Game

---
+++ Assets/Du lieu UI/Menugame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("HeroScene"); // Đổi tên theo scene game thật của bạn
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
